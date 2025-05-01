dbd89c9fef97fb65be5550ca91e1c741c1d23c99
sachinscripts <sachinn3283@gmail.com>
2025-05-01 03:50:03 +0000

    few enhancements on game board

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
+import { useState } from "react"
 import Player from "./components/Player"
-
+import GameBoard from "./components/GameBoard"
 
 function App() {
-  
+    const [activePLayer, setActivePlayer] = useState('X')
+
+    function handleSelectSquare(){
+      setActivePlayer((curActivePlayer)=> curActivePlayer === 'X' ? 'O' : 'X');
+    }
 
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player initialName="Player 1" symbol="X"/>
-          <Player initialName="Player 2" symbol="O"/>
+        <ol id="players" className="highlight-player">
+          <Player initialName="Player 1" symbol="X" isActive={activePLayer === 'X'}/>
+          <Player initialName="Player 2" symbol="O" isActive={activePLayer === 'O'}/>
         </ol>
+        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePLayer}/>
       </div>
-      LOG
     </main>
    
   )
