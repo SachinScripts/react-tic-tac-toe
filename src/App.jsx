257dc3302b6a2ee168dfba4422b24a6139698610
sachinscripts <sachinn3283@gmail.com>
2025-05-03 06:25:26 +0000

    Reduce state management

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,26 @@ import Player from "./components/Player"
 import GameBoard from "./components/GameBoard"
 import Log from "./components/Log"
 
+function deriveActivePlayer(gameTurns){
+  let currentPlayer = 'X';
+
+  if(gameTurns.length > 0 && gameTurns[0].player === 'X'){
+    currentPlayer = 'O';
+  }
+  return currentPlayer;
+}
+
+
 function App() {
     const [gameTurns, setGameTurns] = useState([]);
-    const [activePLayer, setActivePlayer] = useState('X')
+    //const [activePLayer, setActivePlayer] = useState('X')
 
+    const activePlayer = deriveActivePlayer(gameTurns);
     function handleSelectSquare(rowIndex, colIndex){
-      setActivePlayer((curActivePlayer)=> curActivePlayer === 'X' ? 'O' : 'X');
+      //setActivePlayer((curActivePlayer)=> curActivePlayer === 'X' ? 'O' : 'X');
       setGameTurns(prevTurns => {
-        let currentPlayer = 'X';
+       const currentPlayer = deriveActivePlayer(prevTurns);
 
-        if(prevTurns.length > 0 && prevTurns[0].player === 'X'){
-          currentPlayer = 'O';
-        }
         const updatedTurns = [
           {square: {row: rowIndex, col: colIndex}, player: currentPlayer}, 
           ...prevTurns];
@@ -26,8 +34,8 @@ function App() {
     <main>
       <div id="game-container">
         <ol id="players" className="highlight-player">
-          <Player initialName="Player 1" symbol="X" isActive={activePLayer === 'X'}/>
-          <Player initialName="Player 2" symbol="O" isActive={activePLayer === 'O'}/>
+          <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'}/>
+          <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'}/>
         </ol>
         <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns}/>
       </div>
